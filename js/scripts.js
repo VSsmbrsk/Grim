5ba6c1b1bb8d055e63b0dd2d190e6cfd8cc068fd
Vssmbrsk <kolhoznikmarek@gmail.com>
2025-09-17 17:45:44 +0300

    added new project

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const id = this.getAttribute('href').substring(1);
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+ 
+  const popup = document.getElementById('popup');
+  const close = document.querySelector('.popup__close');
+  
+  document.querySelectorAll('.btn-yellow, .btn-red').forEach(btn => {
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+      popup.style.display = 'flex';
+    });
+  });
+  
+  close.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+  
+  popup.addEventListener('click', e => {
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
